1dbaca8ee1926cfb8f9060b7254e183464b361c6
Zygy93 <amazinzygy@gmail.com>
2018-04-10 23:37:49 -0500

    sourced in student services and controllers

---
+++ server/public/scripts/services/student.service.js
@@ -0,0 +1,6 @@
+myApp.service('StudentService', ['$http', '$location', 'UserService', function($http, $location, UserService){
+    console.log('StudentService Loaded');
+    var self = this;
+
+
+}]); // end student service
