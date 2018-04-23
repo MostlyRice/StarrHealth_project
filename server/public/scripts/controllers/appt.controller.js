d4577aeb41a35f93b9b2716680ef5bf5fc489a60
Daniel Shugarts <daniel.shugarts@gmail.com>
2018-04-23 11:00:03 -0500

    notes complete

---
+++ server/public/scripts/controllers/appt.controller.js
@@ -0,0 +1,35 @@
+myApp.controller('ApptController', ['$http', 'UserService', '$location', 'StudentService', 'SignupService', function ($http, UserService, $location, StudentService, SignupService) {
+    console.log('ApptController created');
+    var self = this;
+    self.userService = UserService;
+    self.studentService = StudentService;
+    self.userObject = UserService.userObject;
+    self.id = UserService.userObject.id;
+    self.student = StudentService.student;
+    self.newPassword = StudentService.newPassword;
+    self.changePassword = StudentService.changePassword;
+    self.returnFromPass = StudentService.returnFromPass;
+    self.saveBio = StudentService.saveBio;
+    self.getMyCoach = SignupService.getMyCoach;
+    self.appointment = StudentService.appointment;
+    self.myPastAppts = StudentService.myPastAppts;
+    self.myAppts = StudentService.myAppts;
+    
+  
+  
+    self.studentHome = StudentService.studentHome;
+    self.studentCoach = StudentService.studentCoach;
+    self.studentSchedule = StudentService.studentSchedule;
+  
+    StudentService.myPastAppts();
+
+  
+    self.cancelBio = function () {
+      self.editing = false;
+    }
+  
+    self.editBio = function () {
+      self.editing = true;
+    } 
+  
+  }]);
