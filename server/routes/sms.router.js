bb4479cc322ceef400485ed957f554a1b125e6b3
Zygy93 <amazinzygy@gmail.com>
2018-05-04 14:17:27 -0500

    FINAL CHANGE

--- server/routes/sms.router.js
+++ server/routes/sms.router.js
@@ -52,7 +52,7 @@ router.get('/coachphone/:id', (request, response) => {
     to: phone,
     // Must Insert Phone NUmber From Twilio Account On Line 54 between the '+1'
     // i.e. from: '+15555555555',
-    from: '+1',
+    from: '+19528568083 ',
     body: entry.first_name + ' ' + entry.last_name + ':  ' + entry.newmessage,
   })
   .then(message => console.log(message.sid));
